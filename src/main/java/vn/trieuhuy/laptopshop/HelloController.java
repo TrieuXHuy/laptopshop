b11b08f044f46385b2165f89efaef38525637fc5
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-08-27 15:52:43 -0700

    update pom

---
+++ src/main/java/vn/trieuhuy/laptopshop/HelloController.java
@@ -0,0 +1,12 @@
+package vn.trieuhuy.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
