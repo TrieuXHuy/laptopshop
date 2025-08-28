257e7db65c3836d25a92573dcd33665d497f2090
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-08-28 15:29:08 +0700

    update

--- src/main/java/vn/trieuhuy/laptopshop/HelloController.java
+++ src/main/java/vn/trieuhuy/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
     public String index() {
         return "Hello World";
     }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Only user access this page";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Only admin access this page";
+    }
 }
--- src/main/java/vn/trieuhuy/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/trieuhuy/laptopshop/LaptopshopApplication.java
@@ -2,6 +2,7 @@ package vn.trieuhuy.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
