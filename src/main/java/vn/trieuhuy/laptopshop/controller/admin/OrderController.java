380a6d79f90410617f488491b4b09c87070ae83e
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-09-05 10:22:03 +0700

    create layout

---
+++ src/main/java/vn/trieuhuy/laptopshop/controller/admin/OrderController.java
@@ -0,0 +1,13 @@
+package vn.trieuhuy.laptopshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OrderController {
+
+    @GetMapping("/admin/order")
+    public String getProduct() {
+        return "admin/order/show";
+    }
+}
